c69b5defde269f94228075c044f314495d694e27
Ravi Teja Gannavarapu <IamRaviTejaG@users.noreply.github.com>
2016-12-04 09:45:00 +0530

    Update 01 - 03 - Day of a given date.c

--- C-Lab-Assignment/01 - 03 - Day of a given date.c
+++ C-Lab-Assignment/01 - 03 - Day of a given date.c
@@ -6,20 +6,29 @@ main()
     int date, mon, year, i, s = 0;
     char week[7][10];
     system("cls");
-    strcpy(week[0], "Sunday");
-    strcpy(week[1], "Monday");
-    strcpy(week[2], "Tuesday");
-    strcpy(week[3], "Wednesday");
-    strcpy(week[4], "Thursday");
-    strcpy(week[5], "Friday");
-    strcpy(week[6], "Saturday");
+    strcpy(week[0], "Monday");
+    strcpy(week[1], "Tuesday");
+    strcpy(week[2], "Wednesday");
+    strcpy(week[3], "Thursday");
+    strcpy(week[4], "Friday");
+    strcpy(week[5], "Saturday");
+    strcpy(week[6], "Sunday");
     printf("Enter a valid date (dd/mm/yyyy): ");
     scanf("%d / %d / %d", &date, &mon, &year);
-    if((year % 4 == 0))
-        month[1] = 29;
-    for(i = 0; i < mon - 1; i++)
-        s = s + month[i];
-    s = s + (date + year + (year / 4) - 2);
-    s = s % 7;
+    for (i=1; i<year; i++)
+    {
+        if((year % 4) == 0 && (year % 100) == 0 && (year % 400) == 0)
+            s += 366;
+        else
+            s += 365;
+    }
+    for (i=0; i<mon-1; i++)
+    {
+        if((year % 4) == 0 && (year % 100) == 0 && (year % 400) == 0)
+            month[1] = 29;
+        s += month[i];
+    }
+    s += date;
+    s = s%7;
     printf("\nThe day is: %s", week[s]);
 }
